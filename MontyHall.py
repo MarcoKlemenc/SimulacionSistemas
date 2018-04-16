de7980dc01acc99a8ba9e2a55b0afdce5995625e
gabrielpalacios <palaciosgabriel@hotmail.com>
2018-04-16 10:31:01 -0300

    Update MontyHall.py
    muestra porcentajes al final

--- MontyHall.py
+++ MontyHall.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from random import randint
 
 RONDAS = 10000
-
+print("RONDAS: " + str(RONDAS))
 resultados = defaultdict(int)
 cantidad = 0
 
@@ -22,5 +22,14 @@ for i in range(RONDAS):
         while eleccion == eleccion_antigua and eleccion == descubierta:
             eleccion = randint(0, 2)
     resultados[(puertas[eleccion] == 'COCHE', cambiar)] += 1
+
+a=resultados[(True,0)]
+b=resultados[(False,1)]
+c=resultados[(True,1)]
+d=resultados[(False,0)]
+
 for k, v in sorted(resultados.items(), key=lambda l: (l[0][1], l[0][0])):
-    print ("{} al {}cambiar: {}".format("Victorias" if k[0] else "Derrotas", "no " if not k[1] else "", v))
+    print("{} al {}cambiar: {}".format("Victorias" if k[0] else "Derrotas", "no " if not k[1] else "", v))
+
+print("Porcentaje Victorias al NO haber cambiado: " +  str(d) + " / " + str(a+d) + " = " + str((d/(a+d))*100))
+print("Porcentaje Victorias al SI haber cambiado: " +  str(c) + " / " + str(b+c) + " = " + str((c/(b+c))*100))
