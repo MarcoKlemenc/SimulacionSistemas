90946d131a503c67018b94a959c62338f5f2d8d6
Marco Klemenc <mklemenc@openpyme.com.ar>
2018-05-06 22:25:39 -0300

    Se rearma para hacer búsquedas parciales

--- Patrones.py
+++ Patrones.py
@@ -1,5 +1,11 @@
 import sys
 
 s = sys.argv[1]
-i = (s+s).find(s, 1, -1)
-print (None if i == -1 else s[:i])
+new_s = s
+while(len(new_s) >= len(s)/2):
+    i = (new_s+new_s).find(new_s, 1, -1)
+    a = None if i == -1 else s[:i]
+    if a:
+        print (a)
+        break
+    new_s = new_s[:-1]
